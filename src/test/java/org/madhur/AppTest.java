d0e26861e0f05e16cd869c4d163c0002131888ab
madhurchaurasia <madhur.chaurasia@chownow.com>
2024-01-27 22:56:53 -0700

    selinium POC is done

--- src/test/java/org/madhur/AppTest.java
+++ src/test/java/org/madhur/AppTest.java
@@ -1,38 +1,55 @@
 package org.madhur;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class AppTest {
+
+    WebDriver driver;
+
+    @Test
+    public void test() throws InterruptedException, IOException {
+        System.out.println("Creating Chrome Driver");
+        // Set Chrome Driver
+        //System.setProperty("webdriver.chrome.driver", "/Users/madhurchaurasia/Downloads/chromedriver");
+        //System.setProperty("webdriver.chrome.whitelistedIps", "");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("http://www.google.com");
+        System.out.println("Wait a bit for the page to render");
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("Taking Screenshot");
+        File outputFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String imageDetails = "/Users/madhurchaurasia/Documents/Programming/intelliJ_java_test/selinium-test/src/screenshots/img.png";
+        File screenShot = new File(imageDetails).getAbsoluteFile();
+        FileHandler.copy(outputFile, screenShot);
+        System.out.println("Screenshot saved: {}" + imageDetails);
+        driver.quit();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void verifyTitle() {
+        //WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("headless");
+        driver=new ChromeDriver(options);
+        driver.get("https://www.browserstack.com/");
+        System.out.println("Title is: " +driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
+        driver.quit();
     }
 }
